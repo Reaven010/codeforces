1dff6e595135f9b1404452978c197047074b8cb0
Reaven010 <shubhtiwari19419@gmail.com>
2026-07-23 23:17:38 +0530

    50A Domino Piling

---
+++ 50A. Domino Piling/solution.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        int m,n;
+        cin>>m>>n;
+        cout<<(m*n)/2;
+    }
+    return 0;
+}
